a1d5a2363108305d781973e7364f57fe0e1e9860
Forrest Desjardins <desjardinsfg@gmail.com>
2017-02-05 17:21:28 -0900

    Scrape search page for current cycle

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ chartSupplements.list = (icaos, options = {}) => {
   return listOne(icaos)
 }
 
+const fetchCurrentCycle = request('https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dafd/search/')
+  .then(res => {
+    const $ = cheerio.load(res.body)
+    return $('select#cycle > option:contains(Current)').val()
+  })
+
 const listOne = (icao) => {
-  return request(`https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dafd/search/results/?cycle=1701&ident=${icao}&navaid=`)
-    .then(res => parse(res.body))
+  return fetchCurrentCycle.then(searchCycle => {
+    return request(`https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dafd/search/results/?cycle=${searchCycle}&ident=${icao}&navaid=`)
+      .then(res => parse(res.body))
+  })
 }
 
 // Parse the response HTML

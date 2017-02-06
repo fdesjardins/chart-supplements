a01e7fdbadf1ce66793d96650beaa15365761f59
Forrest Desjardins <desjardinsfg@gmail.com>
2017-02-05 17:22:09 -0900

    Ensure chart supplements are actually received in tests

--- test.js
+++ test.js
@@ -1,21 +1,30 @@
 /* global describe, it */
 
+const assert = require('chai').assert
 const chartSupplements = require('./index')
 
 describe('notams', () => {
-  it('should fetch chart supplements for a single ICAO', (done) => {
-    chartSupplements('KFDC').then(() => done())
+  it('should fetch chart supplements for a single ICAO', () => {
+    return chartSupplements('PANC').then(cs => {
+      assert(cs)
+    })
   })
 
-  it('should fetch NOTAMs for an array of ICAOs', (done) => {
-    chartSupplements(['KFDC', 'KZBW']).then(() => done())
+  it('should fetch NOTAMs for an array of ICAOs', () => {
+    return chartSupplements(['PANC', 'PABV']).then(cs => {
+      assert(cs.length === 2)
+      cs.map(assert)
+    })
   })
 
-  it('should expose the list method', (done) => {
-    chartSupplements.list('KFDC').then(() => done())
+  it('should expose the list method', () => {
+    return chartSupplements.list('PANC')
   })
 
-  it('should fetch NOTAMs for an array of ICAOs using the list method', (done) => {
-    chartSupplements.list(['KFDC', 'KZBW']).then(() => done())
+  it('should fetch NOTAMs for an array of ICAOs using the list method', () => {
+    return chartSupplements.list(['PANC', 'PABV']).then(cs => {
+      assert(cs.length === 2)
+      cs.map(assert)
+    })
   })
 })

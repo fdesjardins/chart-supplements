541028b0cfc7cc909160133fd77bb7b047a26568
Forrest Desjardins <desjardinsfg@gmail.com>
2017-03-07 21:02:36 -0900

    Add test for fetchCurrentCycle

--- test.js
+++ test.js
@@ -3,14 +3,14 @@
 const assert = require('chai').assert
 const chartSupplements = require('./index')
 
-describe('notams', () => {
+describe('chart supplements', () => {
   it('should fetch chart supplements for a single ICAO', () => {
     return chartSupplements('PANC').then(cs => {
       assert(cs)
     })
   })
 
-  it('should fetch NOTAMs for an array of ICAOs', () => {
+  it('should fetch chart supplements for an array of ICAOs', () => {
     return chartSupplements(['PANC', 'PABV']).then(cs => {
       assert(cs.length === 2)
       cs.map(assert)
@@ -21,7 +21,13 @@ describe('notams', () => {
     return chartSupplements.list('PANC')
   })
 
-  it('should fetch NOTAMs for an array of ICAOs using the list method', () => {
+  it('should expose the fetchCurrentCycle method', () => {
+    return chartSupplements.fetchCurrentCycle().then(cycle => {
+      assert(parseInt(cycle))
+    })
+  })
+
+  it('should fetch chart supplements for an array of ICAOs using the list method', () => {
     return chartSupplements.list(['PANC', 'PABV']).then(cs => {
       assert(cs.length === 2)
       cs.map(assert)

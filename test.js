22d6345b02aec548e5f8d0f8373516f86462460c
Forrest Desjardins <desjardinsfg@gmail.com>
2020-05-29 19:12:45 -0400

    Use superstruct to improve tests and remove chai lib

--- test.js
+++ test.js
@@ -1,19 +1,37 @@
 /* global describe, it */
 
-const assert = require('chai').assert
+const assert = require('assert')
+const { struct } = require('superstruct')
 const chartSupplements = require('./index')
+
+const Result = struct({
+  ident: 'string',
+  city: 'string',
+  state: 'string',
+  airport: 'string',
+  navAid: 'string',
+  chart: 'string',
+  volBackPages: {
+    name: 'string',
+    url: 'string'
+  },
+  airportNavAidListing: {
+    name: 'string',
+    url: 'string'
+  }
+})
 
 describe('chart supplements', () => {
   it('should fetch chart supplements for a single ICAO', () => {
     return chartSupplements('PANC').then(cs => {
-      assert(cs)
+      Result(cs)
     })
   })
 
   it('should fetch chart supplements for an array of ICAOs', () => {
     return chartSupplements(['PANC', 'PABV']).then(cs => {
-      assert(cs.length === 2)
-      cs.map(assert)
+      assert.strictEqual(cs.length, 2)
+      cs.map(cs => Result(cs))
     })
   })
 
@@ -29,8 +47,8 @@ describe('chart supplements', () => {
 
   it('should fetch chart supplements for an array of ICAOs using the list method', () => {
     return chartSupplements.list(['PANC', 'PABV']).then(cs => {
-      assert(cs.length === 2)
-      cs.map(assert)
+      assert.strictEqual(cs.length, 2)
+      cs.map(cs => Result(cs))
     })
   })
 })
